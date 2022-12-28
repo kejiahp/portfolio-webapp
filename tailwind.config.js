3ef011e6f7df18962a145a28f4b0af2c8882601b
kejiahp <popoolakejiah@gmail.com>
2022-12-28 13:24:21 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{ts,js,tsx,jsx}', './components/**/*.{ts,js,tsx,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
