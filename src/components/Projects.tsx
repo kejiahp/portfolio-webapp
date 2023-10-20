e04b40ae7dc336d69631f9cfa4a1e9b794509f04
elijah <popoolakejiah@gmail.com>
2023-10-20 10:49:06 +0100

    tomations added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,131 +10,165 @@ import TodoList from "../assets/TodoList.png";
 import EmetricsSuite from "../assets/EmetricsSuite.png";
 import KilogramChatApp from "../assets/KilogramChatApp.png";
 import ManHouse from "../assets/ManHouse.png";
+import TomationSolution from "../assets/TomationSolution.png";
 
 const Projects = () => {
   return (
-    <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16">
-      <SingleProject
-        image={MyPorfolio}
-        githubLink={"https://github.com/kejiahp/portfolio-webapp"}
-        projectLink={"https://portfolio-webapp-lime.vercel.app/"}
-        name="My Porfolio"
-        techUsed={[
-          "Nextjs ",
-          "MongoDb ",
-          "React-Query ",
-          "AOS ",
-          "Axios ",
-          "Nodemailer ",
-          "Yup ",
-        ]}
-      />
+    <>
+      <h1
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-center"
+        className="text-2xl text-white font-bold"
+      >
+        Collaborative Project Contributions
+      </h1>
+      <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-4">
+        <SingleProject
+          image={TomationSolution}
+          githubLink={""}
+          projectLink={"https://www.tomationng.com"}
+          name="Tomation Solutions"
+          techUsed={["React ", "Styled Components ", "React Router Dom"]}
+        />
 
-      <SingleProject
-        image={KilogramChatApp}
-        githubLink={"https://github.com/kejiahp/next13-realtime-chatapp"}
-        projectLink={"https://kilogramchatting.vercel.app"}
-        name="Kilogram Realtime Chatting"
-        techUsed={["React ", "Nextjs ", "Tailwind ", "Socket.io", "Zod", "SWR"]}
-      />
+        <SingleProject
+          image={SequentialJobs}
+          githubLink={""}
+          projectLink={"https://www.sequentialjobs.com"}
+          name="Sequential Jobs"
+          techUsed={[
+            "React ",
+            "Nextjs ",
+            "Stitches ",
+            "Django ",
+            "Postgres ",
+            "Django Rest Framework",
+          ]}
+        />
 
-      <SingleProject
-        image={TylerMovies}
-        githubLink={""}
-        projectLink={"https://tylermoviesempire.com"}
-        name="Tyler Movies Website"
-        techUsed={["React ", "Nextjs ", "Tailwind ", "Mongoose ", "MongoDB"]}
-      />
+        <SingleProject
+          image={ManHouse}
+          githubLink={""}
+          projectLink={"https://www.manufacturersnigeria.org"}
+          name="Manufactures Association of Nigeria"
+          techUsed={[
+            "React ",
+            "Material UI ",
+            "Scss ",
+            "Django Rest Framework",
+            "Postgres ",
+            "Cloud computing",
+          ]}
+        />
 
-      <SingleProject
-        image={SequentialJobs}
-        githubLink={""}
-        projectLink={"https://www.sequentialjobs.com"}
-        name="Sequential Jobs"
-        techUsed={[
-          "React ",
-          "Nextjs ",
-          "Stitches ",
-          "Django ",
-          "Postgres ",
-          "Django Rest Framework",
-        ]}
-      />
+        <SingleProject
+          image={EmetricsSuite}
+          githubLink={""}
+          projectLink={"https://emetricsuite.com"}
+          name="Emetrics Suite"
+          techUsed={["React ", "Nextjs ", "Stiches "]}
+        />
 
-      <SingleProject
-        image={ManHouse}
-        githubLink={""}
-        projectLink={"https://www.manufacturersnigeria.org"}
-        name="Manufactures Association of Nigeria"
-        techUsed={[
-          "React ",
-          "Material UI ",
-          "Scss ",
-          "Django Rest Framework",
-          "Postgres ",
-          "Cloud computing",
-        ]}
-      />
+        <SingleProject
+          image={TylerMovies}
+          githubLink={""}
+          projectLink={"https://tylermoviesempire.com"}
+          name="Tyler Movies Website"
+          techUsed={["React ", "Nextjs ", "Tailwind ", "Mongoose ", "MongoDB"]}
+        />
 
-      <SingleProject
-        image={TodoList}
-        githubLink={"https://github.com/kejiahp/date-filtered-todo-list"}
-        projectLink={"https://date-filtered-todo-list.up.railway.app"}
-        name="ToDo List"
-        techUsed={["React ", "Tailwind "]}
-      />
+        <SingleProject
+          image={RaveUniversal}
+          githubLink={"https://github.com/kejiahp/RaveUniversalPolls"}
+          projectLink={"https://theraveuniversal.com/"}
+          name="The Rave Universal"
+          techUsed={["Django ", "Bootstrap ", "Jinja Templates"]}
+        />
+      </section>
 
-      <SingleProject
-        image={EmetricsSuite}
-        githubLink={""}
-        projectLink={"https://emetricsuite.com"}
-        name="Emetrics Suite"
-        techUsed={["React ", "Nextjs ", "Stiches "]}
-      />
+      <h1
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-center"
+        className="text-2xl text-white font-bold mt-8"
+      >
+        Personal Projects
+      </h1>
+      <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16">
+        <SingleProject
+          image={MyPorfolio}
+          githubLink={"https://github.com/kejiahp/portfolio-webapp"}
+          projectLink={"https://portfolio-webapp-lime.vercel.app/"}
+          name="My Porfolio"
+          techUsed={[
+            "Nextjs ",
+            "MongoDb ",
+            "React-Query ",
+            "AOS ",
+            "Axios ",
+            "Nodemailer ",
+            "Yup ",
+          ]}
+        />
 
-      <SingleProject
-        image={RaveUniversal}
-        githubLink={"https://github.com/kejiahp/RaveUniversalPolls"}
-        projectLink={"https://theraveuniversal.com/"}
-        name="The Rave Universal"
-        techUsed={["Django ", "Bootstrap ", "Jinja Templates"]}
-      />
+        <SingleProject
+          image={KilogramChatApp}
+          githubLink={"https://github.com/kejiahp/next13-realtime-chatapp"}
+          projectLink={"https://kilogramchatting.vercel.app"}
+          name="Kilogram Realtime Chatting"
+          techUsed={[
+            "React ",
+            "Nextjs ",
+            "Tailwind ",
+            "Socket.io",
+            "Zod",
+            "SWR",
+          ]}
+        />
 
-      <SingleProject
-        image={MarketColonyBot}
-        githubLink={"https://github.com/kejiahp/market-colony-chatbot"}
-        projectLink={""}
-        name="FaceBook ChatBot"
-        techUsed={["Typescript ", "Nodejs ", "MongoDB ", "Express ", "EJS "]}
-      />
+        <SingleProject
+          image={TodoList}
+          githubLink={"https://github.com/kejiahp/date-filtered-todo-list"}
+          projectLink={"https://date-filtered-todo-list.up.railway.app"}
+          name="ToDo List"
+          techUsed={["React ", "Tailwind "]}
+        />
 
-      <SingleProject
-        image={ElectroEcom}
-        githubLink={
-          "https://github.com/kejiahp/electro-electronics-store-react-app"
-        }
-        projectLink={""}
-        name="Electro Ecommerce Website"
-        techUsed={[
-          "React ",
-          "React-Router ",
-          "Styled-Components ",
-          "React-Query ",
-        ]}
-      />
+        <SingleProject
+          image={MarketColonyBot}
+          githubLink={"https://github.com/kejiahp/market-colony-chatbot"}
+          projectLink={""}
+          name="FaceBook ChatBot"
+          techUsed={["Typescript ", "Nodejs ", "MongoDB ", "Express ", "EJS "]}
+        />
 
-      <div data-aos="fade-up">
-        <h1 className="font-bold md:text-5xl text-3xl text-custGreen hover:text-custBlack-400 transition-all duration-200 ease-in">
-          <a
-            target={"_blank"}
-            rel="noreferrer"
-            href="https://github.com/kejiahp"
-          >
-            ViEw RePOSiTOrY
-          </a>
-        </h1>
-      </div>
-    </section>
+        <SingleProject
+          image={ElectroEcom}
+          githubLink={
+            "https://github.com/kejiahp/electro-electronics-store-react-app"
+          }
+          projectLink={""}
+          name="Electro Ecommerce Website"
+          techUsed={[
+            "React ",
+            "React-Router ",
+            "Styled-Components ",
+            "React-Query ",
+          ]}
+        />
+
+        <div data-aos="fade-up">
+          <h1 className="font-bold md:text-5xl text-3xl text-custGreen hover:text-custBlack-400 transition-all duration-200 ease-in">
+            <a
+              target={"_blank"}
+              rel="noreferrer"
+              href="https://github.com/kejiahp"
+            >
+              ViEw RePOSiTOrY
+            </a>
+          </h1>
+        </div>
+      </section>
+    </>
   );
 };
 
